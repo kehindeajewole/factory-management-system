6f2558308ab4f618f468892aea790bf14adda06a
kehindeajewole <100034733+kehindeajewole@users.noreply.github.com>
2022-09-07 09:31:33 +0100

    Add files via upload

--- path.php
+++ path.php
@@ -1,4 +1,5 @@
 <?php
 define('ABSPATH', __DIR__ . '/');
-define('BASE_URL', "http://localhost/factory-management-system");
+//define('BASE_URL', "http://localhost/factory-management-system");
+define('BASE_URL', "https://factory-management-system-app.herokuapp.com");
 define('PATH', dirname(__FILE__));
